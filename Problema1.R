c7bc029319fa66414075d04dd7586b9fe098cf60
daniel.munoz.sanchez <daniel.munoz.sanchez@auleseebe.upc.edu>
2025-02-13 13:07:33 +0100

    sasa

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,22 @@
 #Codigo para problema 1
+mtcars
+dim(mtcars)
+mtcars$qsec
+mis_dades <-mtcars
+mis_dades
+x<- mis_dades$qsec
+sum(x)/length(x) #mitja
 
-plot(1:5)
+x<- mis_dades$drat
+sort(x)
+median(x)
+quantile(x,0.25)
+quantile(x,0.75)
+quantile(mis_dades$mpg,0.18)
+quantile(mis_dades$mpg,0.75)
+IQR(mis_dades$mpg)
+boxplot(mis_dades$mpg)
+mean(mis_dades$cyl)
+IQR(mis_dades$cyl)
+sd(mis_dades$cyl)
+var(mis_dades$qsec)
